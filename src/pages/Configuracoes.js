b316c37f34a90e6a413c4f124068523eb1ad73b7
vitorGaia <vitorgaia0027@gmail.com>
2023-04-20 18:05:35 -0300

    req 9 em progresso

--- src/pages/Configuracoes.js
+++ src/pages/Configuracoes.js
@@ -4,12 +4,12 @@ class Configuracoes extends Component {
   render() {
     return (
       <div>
-        <title
-          data-testid="settings-title"
-        >
-          <h1>Configurações</h1>
-
-        </title>
+        <h1 data-testid="settings-title">Configurações</h1>
+        <select>
+          <option>Easy</option>
+          <option>Medium</option>
+          <option>Hard</option>
+        </select>
       </div>
     );
   }
--- src/redux/reducers/player.js
+++ src/redux/reducers/player.js
@@ -7,7 +7,6 @@ const INITIAL_STATE = {
 };
 
 const player = (state = INITIAL_STATE, action) => {
-  console.log(action);
   switch (action.type) {
   case PLAYER:
     return {
