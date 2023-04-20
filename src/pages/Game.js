71d746683797a52ba6e89b1ee2a484d9c7f27428
Luar <luarrezende@gmail.com>
2023-04-20 15:44:50 -0300

    inicia req 13

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { returnTokenLocalStorge } from '../services/token';
 import Header from '../components/Header';
+import { addAssertions } from '../redux/actions';
 
 export default class Game extends Component {
   state = {
@@ -28,6 +29,17 @@ export default class Game extends Component {
     }
   };
 
+  answer = (param) => {
+    const { dispatch } = this.props;
+    if (param === 'correto') {
+      const number = 1;
+      dispatch(addAssertions(number));
+    } else {
+      const number = 0;
+      dispatch(addAssertions(number));
+    }
+  };
+
   renderQuestion = (index) => {
     const { questions } = this.state;
     const question = questions[index] || {};
@@ -39,7 +51,12 @@ export default class Game extends Component {
     const answersBtns = allAnswers.map((answer, indexAnswers) => {
       if (indexAnswers === 0) {
         return (
-          <button key="#" data-testid="correct-answer" id={ indexAnswers }>
+          <button
+            key="#"
+            data-testid="correct-answer"
+            id={ indexAnswers }
+            onClick={ () => this.answer('correto') }
+          >
             {answer}
           </button>
         );
@@ -49,6 +66,7 @@ export default class Game extends Component {
           key={ indexAnswers }
           data-testid={ `wrong-answer-${indexAnswers - 1}` }
           id={ indexAnswers }
+          onClick={ () => this.answer('errado') }
         >
           {answer}
         </button>
@@ -82,4 +100,5 @@ Game.propTypes = {
   history: PropTypes.shape({
     push: PropTypes.func.isRequired,
   }).isRequired,
+  dispatch: PropTypes.func.isRequired,
 };
